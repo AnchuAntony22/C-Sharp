52b8dec6cc3f95f0aff497dd19dce858fc7f3fab
Anchu Antony <anchuantony2212@gmail.com>
2024-05-02 11:23:22 +0200

    new files

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Exercise 1: Method to return an empty int array of random size between 2 and 25
+        int[] randomArray = GenerateRandomIntArray();
+        Console.WriteLine("Random Array Length: " + randomArray.Length);
+
+        // Exercise 2: Fill an int array of size 8 with random numbers between 0 and 5
+        int[] filledArray = new int[8];
+        Random rand = new Random();
+        for (int i = 0; i < filledArray.Length; i++)
+        {
+            filledArray[i] = rand.Next(0, 6); // Generates a random number between 0 and 5
+        }
+        Console.WriteLine("Filled Array: " + string.Join(", ", filledArray));
+
+        // Exercise 3: Replace a random element of a string array with "_"
+        string[] stringArray = { "overwrite", "depending", "incorrect", "endpoint", "using", "document" };
+        int randomIndex = rand.Next(0, stringArray.Length); // Generates a random index
+        stringArray[randomIndex] = "_";
+        Console.WriteLine("Modified String Array: " + string.Join(", ", stringArray));
+    }
+
+    static int[] GenerateRandomIntArray()
+    {
+        Random rand = new Random();
+        int size = rand.Next(2, 26); // Generates a random size between 2 and 25
+        return new int[size];
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Exercise 1: Change a string value into a string array
+        string text1 = "If we want to restrict this feature, we need to set a flag";
+        string[] array1 = text1.Split(' ');
+        Console.WriteLine("Array 1: " + string.Join(", ", array1));
+
+        // Exercise 2: Change a string value into a string array separated by commas
+        string text2 = "If,we,want,to,restrict,this,feature,we,need,to,set,a,flag";
+        string[] array2 = text2.Split(',');
+        Console.WriteLine("Array 2: " + string.Join(", ", array2));
+
+        // Exercise 3: Change a string value into a string array without spaces
+        string text3 = " If , we ,want,to, restrict , this , feature,we, need ,to ,set,a, flag ";
+        string[] array3 = text3.Trim().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+        Console.WriteLine("Array 3: " + string.Join(", ", array3));
+
+        // Exercise 4: Remove the first three words from a string
+        string text4 = "If we want to restrict this feature, we need to set a flag";
+        string newText = text4.Substring(text4.IndexOf(' ') + 1).Substring(text4.IndexOf(' ') + 1).Substring(text4.IndexOf(' ') + 1);
+        Console.WriteLine("New Text: " + newText);
+
+        // Exercise 5: Find the index of the first occurrence of 's'
+        string text5 = "If we want to restrict this feature, we need to set a flag";
+        int index = text5.IndexOf('s');
+        Console.WriteLine("Index of 's': " + index);
+    }
+}
