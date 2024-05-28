b858b7450adfc471fb3c66f4c0f75d0fe7cad2c4
Anchu Antony <94461233+AnchuAntony22@users.noreply.github.com>
2024-05-28 13:15:46 +0200

    Delete WebApplicationAss1 directory

--- WebApplicationAss1/WebApplicationAss1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplicationAss1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
