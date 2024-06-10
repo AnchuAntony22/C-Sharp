5c1afc611d6e94a99d881a6e9810554101186760
Anchu Antony <anchuantony2212@gmail.com>
2024-06-10 13:52:17 +0200

    MVC Company Ass1

---
+++ WebApplicationCompany/WebApplicationCompany/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationCompany.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
